91d8bec923a240ac8671adf2e77c815bd331c25f
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2025-07-21 16:29:08 +0600

    Ignore transactions with invalid data

--- Sources/Eip20Kit/Contracts/ApproveMethodFactory.swift
+++ Sources/Eip20Kit/Contracts/ApproveMethodFactory.swift
@@ -6,6 +6,9 @@ class ApproveMethodFactory: IContractMethodFactory {
     let methodId: Data = ContractMethodHelper.methodId(signature: ApproveMethod.methodSignature)
 
     func createMethod(inputArguments: Data) throws -> ContractMethod {
+        guard inputArguments.count >= 64 else {
+            throw ContractMethodFactories.DecodeError.invalidABI
+        }
         let spender = Address(raw: inputArguments[12 ..< 32])
         let value = BigUInt(inputArguments[32 ..< 64])
 
